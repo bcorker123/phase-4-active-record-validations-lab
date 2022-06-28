939f4fced557faaab1d1b10913e1d45c4e7e2e48
brian corker <bcorker123@gmail.com>
2022-06-28 15:33:52 -0400

    commit tree 5fd37a3259942a5a2347f9f730969c7f59c2e03b

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validate :clickbait_title 
+    validates :content, length: {minimum:250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in:%w(Fiction Non-Fiction)}
+
+    def clickbait_title
+        unless title.match?(/"Won't Believe"/)
+            errors.add(:title, "Title must be clickbait")
+        end
+    end
 end
